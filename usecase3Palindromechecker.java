f9860d2b30b801856e03c930b4744fbce2350f96
dhoniv <dhonivvasanth@gmail.com>
2026-03-11 09:27:28 +0530

    Add files via upload

---
+++ usecase3Palindromechecker.java
@@ -0,0 +1,22 @@
+public class usecase3Palindromechecker {
+
+
+        public static void main(String[] args) {
+
+            String input = "level";
+            String reversed = "";
+
+            // reverse the string
+            for (int i = input.length() - 1; i >= 0; i--) {
+                reversed = reversed + input.charAt(i);
+            }
+
+            // compare original and reversed
+            if (input.equals(reversed)) {
+                System.out.println(input + " is a Palindrome");
+            } else {
+                System.out.println(input + " is Not a Palindrome");
+            }
+
+        }
+    }
